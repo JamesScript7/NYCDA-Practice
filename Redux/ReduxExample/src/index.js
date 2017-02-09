5aa2b218948279a9d2b7ba1e6d7f0b34418ba0ab
Alisher778 <internet-docs@mail.ru>
2017-02-09 10:32:16 -0500

    reducer and dispatch, store are learnt

--- Redux/ReduxExample/src/index.js
+++ Redux/ReduxExample/src/index.js
@@ -1,30 +1,38 @@
 import { combineReducers, createStore } from 'redux';
 
-const usersDefaultValue = {name: 'Alisher', age: 27}
-const userReducer = (state=usersDefaultValue, action) => {
+
+const userInitialValue = {name: "Ali", hobby: "Codding"}
+const userReducer = (state=userInitialValue, action) => {
+  switch(action.type){
+    case 'ADD_NAME':{
+      state = {...state, name: action.newName}
+      break;
+    }
+    case 'ADD_HOBBY':{
+      state = {...state, hobby: action.newHobby}
+      break;
+    }
+  }
+
   return state;
-};
+}
 
 
-const tweetsDefaultValues = {tweet: 'Reaxt is very easy', author: usersDefaultValue.name}
-const tweeetsReducer = (state=tweetsDefaultValues, action) => {
+const hobbyInitialValue = {hobby: "Codding", who: "Ali"}
+const hobbyReducer = (state=hobbyInitialValue, action) => {
   return state;
-};
-
+}
 
 const reducers = combineReducers({
   user: userReducer,
-  tweets: tweeetsReducer
+  hobby: hobbyReducer
 })
 
 const store = createStore(reducers);
 
-
 store.subscribe(()=>{
-  console.log('added something', store.getState());
+  console.log('hello', store.getState());
 });
 
-store.dispatch({
-  type: "ADD",
-  num: 10
-});
+store.dispatch({type: "ADD_NAME", newName: "Alex"})
+store.dispatch({type: "ADD_HOBBY", newHobby: "Running"})
